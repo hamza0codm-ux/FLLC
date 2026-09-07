5ed6edd362879e4f26f86326832f3808a7efae0f
hamza0codm-ux <hamza0codm@gmail.com>
2026-09-07 14:47:29 +0200

    Update ticket.js

--- src/interactions/buttons/ticket/ticket.js
+++ src/interactions/buttons/ticket/ticket.js
@@ -2,18 +2,82 @@ import createTicketHandler, {
   closeTicketHandler,
   claimTicketHandler,
   priorityTicketHandler,
-  pinTicketHandler,
   unclaimTicketHandler,
   reopenTicketHandler,
   deleteTicketHandler,
 } from '../../../handlers/ticketButtons.js';
 
+/*
+|--------------------------------------------------------------------------
+| Legacy ticket button compatibility
+|--------------------------------------------------------------------------
+|
+| Older Fruity ticket panels used:
+|
+| normal_ticket_create:<type>
+| merch_ticket_create:<type>
+|
+| The current handler uses:
+|
+| create_ticket:normal:<type>
+| create_ticket:merch:<type>
+|
+| These aliases keep already-existing Discord panels working without
+| requiring the panel message to be deleted or recreated.
+|
+*/
+
+const normalTicketCreateLegacyHandler = {
+  name: 'normal_ticket_create',
+
+  async execute(interaction, client, args = []) {
+    await createTicketHandler.execute(
+      interaction,
+      client,
+      ['normal', args[0]]
+    );
+  },
+};
+
+const merchTicketCreateLegacyHandler = {
+  name: 'merch_ticket_create',
+
+  async execute(interaction, client, args = []) {
+    await createTicketHandler.execute(
+      interaction,
+      client,
+      ['merch', args[0]]
+    );
+  },
+};
+
 export default [
+  /*
+  |--------------------------------------------------------------------------
+  | Current ticket system
+  |--------------------------------------------------------------------------
+  */
+
   createTicketHandler,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Legacy panel compatibility
+  |--------------------------------------------------------------------------
+  */
+
+  normalTicketCreateLegacyHandler,
+  merchTicketCreateLegacyHandler,
+
+  /*
+  |--------------------------------------------------------------------------
+  | Ticket controls
+  |--------------------------------------------------------------------------
+  */
+
   closeTicketHandler,
   claimTicketHandler,
   priorityTicketHandler,
-  pinTicketHandler,
   unclaimTicketHandler,
   reopenTicketHandler,
   deleteTicketHandler,
