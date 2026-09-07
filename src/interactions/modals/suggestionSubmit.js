915b75e4a7d18a6f43d48b51bbf23b58a9504ed7
hamza0codm-ux <hamza0codm@gmail.com>
2026-09-07 19:17:05 +0200

    Update suggestionSubmit.js

--- src/interactions/modals/suggestionSubmit.js
+++ src/interactions/modals/suggestionSubmit.js
@@ -60,7 +60,7 @@ export default {
             .setColor('#F8D568')
             .setTitle('New Fruity Suggestion')
             .setDescription(
-                `<a:Loading:1546268064008642641> **Status** <a:Loading:1546268064008642641> Pending • <t:${Math.floor(createdAt / 1000)}:F>\n` +
+                `<a:Loading:1546268064008642641> **Status:** <a:Loading:1546268064008642641> Pending • <t:${Math.floor(createdAt / 1000)}:F>\n` +
                 `<:Apply:1329723672801316925> **Action by:** <a:Loading:1546268064008642641> Pending\n\n` +
                 `**From:** <@${interaction.user.id}>\n` +
                 `\`${escapeInlineCode(suggestionText)}\``
