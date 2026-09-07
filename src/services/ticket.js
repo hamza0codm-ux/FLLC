24953f0709199316a0134c256aa9716a9c40b6ff
hamza0codm-ux <hamza0codm@gmail.com>
2026-09-07 21:08:53 +0200

    Update ticket.js

--- src/services/ticket.js
+++ src/services/ticket.js
@@ -1474,7 +1474,7 @@ try {
             .setLabel(
               'Add Comment'
             )
-            .setEmoji('💅')
+            .setEmoji('📨')
             .setStyle(
               ButtonStyle.Secondary
             ),
