6810f089ed1388fa08d8608f8afee31e277b336c
hamza0codm-ux <hamza0codm@gmail.com>
2026-09-07 14:30:59 +0200

    Fix button ID in ticket creation

--- src/tickets/normalTickets.js
+++ src/tickets/normalTickets.js
@@ -139,7 +139,7 @@ export function buildNormalTicketPanel() {
     const ticketButton =
       new ButtonBuilder()
         .setCustomId(
-          `create_ticket:normal:${button.key}`
+          `create_ticket:normal:${button.id}`
         )
         .setLabel(button.label)
         .setStyle(ButtonStyle.Secondary)
