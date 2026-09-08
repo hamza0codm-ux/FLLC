7d5991da77bb54e56aff9e8fc31a27190bffe1da
hamza0codm-ux <hamza0codm@gmail.com>
2026-09-08 22:38:05 +0200

    Update counterButtons.js

--- src/handlers/counterButtons.js
+++ src/handlers/counterButtons.js
@@ -1,6 +1,5 @@
 import { MessageFlags } from 'discord.js';
 import { createEmbed, successEmbed } from '../utils/embeds.js';
-import { performDeletionByCounterId } from '../commands/ServerStats/modules/serverstats_delete.js';
 import { logger } from '../utils/logger.js';
 import { ErrorTypes, replyUserError, handleInteractionError } from '../utils/errorHandler.js';
 
