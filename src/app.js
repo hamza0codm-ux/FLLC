ef90a2d353b5a9ee6dce99c4fa7cd144f671e69d
hamza0codm-ux <hamza0codm@gmail.com>
2026-09-09 10:10:21 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ import { checkBirthdays } from './services/birthdayService.js';
 import { checkGiveaways } from './services/giveawayService.js';
 import { loadCommands, registerCommands as registerSlashCommands } from './handlers/loaders/commandLoader.js';
 import { runSafeTask, handleTaskError, ErrorCodes } from './utils/errorHandler.js';
+import { reconcileSocialsPanel } from './path/to/your/socials-file.js';
 import { initializeMusic } from './services/music/riffySetup.js';
 import { shutdownMusic } from './services/music/playerHandler.js';
 import pkg from '../package.json' with { type: 'json' };
